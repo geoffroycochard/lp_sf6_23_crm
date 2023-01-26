6fa07ef371293715a9317cb120f34f974a7919ff
Geoffroy Cochard <geoffroycochard@gmail.com>
2023-01-26 08:09:54 +0000

    Form opportunity import from class

--- src/Form/OpportunityType.php
+++ src/Form/OpportunityType.php
@@ -3,7 +3,9 @@
 namespace App\Form;
 
 use App\Entity\Opportunity;
+use App\Enum\OpportunityStatus;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EnumType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
